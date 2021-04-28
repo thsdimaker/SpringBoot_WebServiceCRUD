657044400ac7809e10b0f8b996aa66ea458a0d7d
DG <thsdimaker@gmail.com>
2021-04-28 15:47:50 +0900

    Success

--- src/main/java/com/webservicecrud/springboot/web/HelloController.java
+++ src/main/java/com/webservicecrud/springboot/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.webservicecrud.springboot.web;
 
+import com.webservicecrud.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 
 
